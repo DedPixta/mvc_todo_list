20f74151dd5f4dfb828b3ee2eefbd783cabbe487
Maxim Kossatyy <max.kossatyy@gmail.com>
2023-03-08 22:00:54 +0600

    added spring injection taskMapper, in config replaced hibernate properties with constants.

--- src/main/java/com/makos/mvctodolist/service/TaskServiceImpl.java
+++ src/main/java/com/makos/mvctodolist/service/TaskServiceImpl.java
@@ -18,38 +18,39 @@ import org.springframework.transaction.annotation.Transactional;
 public class TaskServiceImpl implements TaskService {
 
     private final TaskDAO taskDAO;
+    private final TaskMapper taskMapper;
 
     @Override
     public TaskDTO findById(Integer id) {
         return taskDAO.findById(id)
-                .map(TaskMapper.INSTANCE::mapToTaskDTO)
+                .map(taskMapper::mapToTaskDTO)
                 .orElseThrow(() -> new TaskNotFoundException("Not found task with ID - " + id));
     }
 
     @Override
     public Page<TaskDTO> findAll(PageRequest pageRequest) {
         return taskDAO.findAll(pageRequest)
-                .map(TaskMapper.INSTANCE::mapToTaskDTO);
+                .map(taskMapper::mapToTaskDTO);
     }
 
     @Transactional
     @Override
     public void save(TaskDTO taskDTO) {
-        Task task = TaskMapper.INSTANCE.mapToTask(taskDTO);
+        Task task = taskMapper.mapToTask(taskDTO);
         taskDAO.save(task);
     }
 
     @Transactional
     @Override
     public void update(TaskDTO taskDTO) {
-        Task task = TaskMapper.INSTANCE.mapToTask(taskDTO);
+        Task task = taskMapper.mapToTask(taskDTO);
         taskDAO.save(task);
     }
 
     @Transactional
     @Override
     public void delete(TaskDTO taskDTO) {
-        Task task = TaskMapper.INSTANCE.mapToTask(taskDTO);
+        Task task = taskMapper.mapToTask(taskDTO);
         taskDAO.delete(task);
     }
 }
